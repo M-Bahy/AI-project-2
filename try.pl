f29511ee27770382f15b3a43db4a38466c2ad420
Omar Shaaban <ofahim2002@gmail.com>
2024-11-17 21:21:44 +0200

    Worked with two variables.

--- try.pl
+++ try.pl
@@ -4,40 +4,41 @@ ids(X,L):-
 (call_with_depth_limit(myPredicate(X),L,R), R=depth_limit_exceeded,
 L1 is L+1, ids(X,L1)).
 
-bottle_layers(N, Color1, Color2, S) :-
-    (
-        % Base case: Initial state (s0)
-        S = s0, 
-        (
-            (N = 1, bottle1(Color1, Color2));
-            (N = 2, bottle2(Color1, Color2));
-            (N = 3, bottle3(Color1, Color2))
-        )
-    );
-    (
-        % Recursive case: Handle the result of pouring
-        S = result(pour(From, To), PreviousSituation),
-        bottle_layers(1, Top1, Bottom1, PreviousSituation),
-        bottle_layers(2, Top2, Bottom2, PreviousSituation),
-        bottle_layers(3, Top3, Bottom3, PreviousSituation),
 
-        % Update the bottles based on the pour action
-        (
-            From = 1, To = 3 -> 
-                (Top1 \= e -> Updated1 = (e, Bottom1), Updated3 = (Bottom3, Top1); 
-                 Bottom1 \= e -> Updated1 = (Top1, e), Updated3 = (Bottom3, Bottom1)),
-                Updated2 = (Top2, Bottom2);
-            % Add other cases if necessary
-            fail
-        ),
 
-        % Assign updated layers based on the bottle index
-        (
-            N = 1 -> Updated1 = (Color1, Color2);
-            N = 2 -> Updated2 = (Color1, Color2);
-            N = 3 -> Updated3 = (Color1, Color2)
-        )
+current_bottle_colors(Top1,Bottom1,Top2,Bottom2,Top3,Bottom3,s0):-
+    bottle1(Top1,Bottom1),
+    bottle2(Top2,Bottom2),
+    bottle3(Top3,Bottom3).
+
+
+current_bottle_colors(Top1, Bottom1, Top2, Bottom2, Top3, Bottom3, result(Action, S)) :-
+    (
+        % Case where the action fails
+        (current_bottle_colors(Top1, Bottom1, Top2, Bottom2, Top3, Bottom3, S),
+        pour(A, B) = Action,
+        \+ can_pour(Top1, Bottom1,Top2,Bottom2,Top3,Bottom3, S))
+    ;
+        % Case where the action succeeds
+        (current_bottle_colors(Top1Prev, Bottom1Prev, Top2Prev, Bottom2Prev, Top3Prev, Bottom3Prev, S),
+        pour(A, B) = Action,
+        can_pour(Top1Prev,Bottom1Prev,Top2Prev,Bottom2Prev,Top3Prev,Bottom3Prev, S),
+        update_bottle_colors(A, B, Top1Prev, Bottom1Prev, Top2Prev, Bottom2Prev, Top3Prev, Bottom3Prev, Top1, Bottom1, Top2, Bottom2, Top3, Bottom3))
     ).
+
+update_bottle_colors(1, 2, Top1Prev, Bottom1Prev, Top2Prev, Bottom2Prev, Top3Prev, Bottom3Prev, Top1, Bottom1, Top2, Bottom2, Top3, Bottom3) :-
+    (Top1Prev \= e -> Top1 = e, Bottom1 = Bottom1Prev, Top3= Top3Prev, Bottom3=Bottom3Prev,
+    (Bottom2Prev \= e -> Top2 = Top1Prev, Bottom2 = Bottom2Prev);
+    (Bottom2Prev = e -> Top2 =e, Bottom2 = Top1Prev));
+
+    (Top1Prev = e -> Top1 = e, Bottom1 = e, Top3 = Top3Prev, Bottom3 = Bottom3Prev,
+    
+    (Bottom2Prev \= e -> Top2 = Top1Prev, Bottom2 = Bottom2Prev);
+    (Bottom2Prev = e -> Top2 =e, Bottom2 = Top1Prev)).
+
+
+
+    
 
 
 add_action(Action, PreviousSituation, NewSituation) :-
@@ -45,9 +46,9 @@ add_action(Action, PreviousSituation, NewSituation) :-
     can_pour(A, B, PreviousSituation),
     NewSituation = result(Action, PreviousSituation).
 
-can_pour(From, To, S) :-
-    bottle_layers(From, Top1, Bottom1, S),
-    bottle_layers(To, Top2, Bottom2, S),
+
+can_pour(Top1,Bottom1,Top2,Bottom2,Top3,Bottom3, S) :-
+   
     Top2 = e,
     (  
         (Top1 \= e , (  (Bottom2 =e)     ;    (Bottom2 \=e , Top1 = Bottom2)   )   )  
@@ -55,6 +56,10 @@ can_pour(From, To, S) :-
         (Bottom1 \= e , (  (Bottom2 =e)     ;    (Bottom2 \=e , Bottom1 = Bottom2)   )   ) 
     ),
     !.
+
+
+
+
 
 % add_action(pour(1,3), [[b,r],[b,r],[e,e]], S).
 
